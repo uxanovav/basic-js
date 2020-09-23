cd3471ee27adec39a9d272991fd4b7cc60d02fe5
Alexander <uxanovav@yandex.ru>
2020-09-23 11:52:08 +0400

    feat: count-cats - complited

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // githubtest;
+module.exports = function countCats(backyard) {
+  const cat = "^^";
+  let count = 0;
+  searchCats(backyard);
+  function searchCats(arr) {
+    for (let i of arr) {
+      if (Array.isArray(i)) {
+        searchCats(i);
+      } else if (i === cat) {
+        count++;
+      }
+    }
+  }
+  return count;
 };
