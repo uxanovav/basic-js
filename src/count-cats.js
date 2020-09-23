4cc2fc0030957539d7a12f765e61e3b120304195
Alexander <uxanovav@yandex.ru>
2020-09-23 10:48:41 +0400

    test: git test

--- src/count-cats.js
+++ src/count-cats.js
@@ -2,5 +2,5 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(/* matrix */) {
   throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  // githubtest
 };
