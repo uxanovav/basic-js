2c6abbd2618b5f97ff28916034f5bc0473c12bda
Alexander <uxanovav@yandex.ru>
2020-09-24 10:01:30 +0400

    feats: what-season - complited

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,6 +1,6 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
+const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(/* sampleActivity */) {
