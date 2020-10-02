26783e569015da14b24c497ab696620ecf43351d
Alexander <uxanovav@yandex.ru>
2020-10-02 16:40:12 +0400

    feats: transform-array complited with 2 errors

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,53 @@
+const { prototype } = require("mocha");
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let result = [];
+  let workCopy = arr;
+  if (!Array.isArray(arr)){
+    throw new Error('Not Array!!');
+  } else {
+    for (let i = 0; i < arr.length; i++){
+      if (arr[i] === '--discard-next'){
+        workCopy[i+1] = 'NaN';
+        workCopy[i] = 'NaN';
+      }
+      if (arr[i] === '--discard-prev')
+        {
+          if (i != 0){
+          workCopy[i-1] = 'NaN';
+          }
+          workCopy[i] = 'NaN';
+        }
+      if (arr[i] === '--double-next'){
+        if (arr.length !== i+1){
+          let sideA = workCopy.slice(0,i)
+          let sideB = workCopy.slice(i+1);
+          sideA.push(arr[i+1]);
+          workCopy = sideA.concat(sideB);
+        } else {
+          workCopy[i] = 'NaN';
+        }
+      }
+      if (arr[i] === '--double-prev'){
+          if (i != 0){
+          let sideA = workCopy.slice(0,i)
+          let sideB = workCopy.slice(i+1);
+          console.log(sideB);
+          sideA.push(workCopy[i-1]);
+          console.log(sideA);
+          workCopy = sideA.concat(sideB);
+          console.log(workCopy);
+         } else {
+          workCopy[i] = 'NaN';
+         }
+      }
+    }
+  }
+  for (let el of workCopy){
+    if (el != 'NaN'){
+      result.push(el);
+    }
+  }
+  return result;
 };
